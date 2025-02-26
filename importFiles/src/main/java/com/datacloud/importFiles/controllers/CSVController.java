8194346a4a2d68214cb5fdb110846c156c8cefe4
Gildo Salvador <gildosalva.pe@gmail.com>
2025-02-26 09:42:58 +0100

    Add CSV controller

--- importFiles/src/main/java/com/datacloud/importFiles/controllers/CSVController.java
+++ importFiles/src/main/java/com/datacloud/importFiles/controllers/CSVController.java
@@ -1,4 +1,25 @@
 package com.datacloud.importFiles.controllers;
 
+import com.datacloud.importFiles.dto.ProductDTO;
+import com.datacloud.importFiles.service.CSVService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/csv")
 public class CSVController {
+
+    private final CSVService csvService;
+
+    public CSVController(CSVService csvService) {
+        this.csvService = csvService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<List<ProductDTO>> uploadCSV(@RequestParam("file") MultipartFile file) {
+        List<ProductDTO> products = csvService.readCSVFile(file);
+        return ResponseEntity.ok(products);
+    }
 }
